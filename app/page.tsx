015a7f97f7dd498696c0458542340212dfe0f002
bastien <bastienandre68@gmail.com>
2025-10-08 13:22:46 +0200

    build fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import BlogSection from "@/components/home/BlogSection";
 import HeroSection from "@/components/home/HeroSection";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
-      <BlogSection />
+      <Suspense fallback={<div>Loading posts...</div>}>
+        <BlogSection />
+      </Suspense>
     </div>
   );
 }
