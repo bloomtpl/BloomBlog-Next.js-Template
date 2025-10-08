d5792695f89229b8c5416a73090d3ee99595ed74
bastien <bastienandre68@gmail.com>
2025-10-08 13:28:45 +0200

    Import Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import Footer from "@/components/layout/Footer";
-import Header from "@/components/layout/Header";
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
+import Footer from "../components/layout/Footer";
+import Header from "../components/layout/Header";
 import "./globals.css";
 
 const outfit = Outfit({
